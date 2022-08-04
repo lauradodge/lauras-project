e9a2b16e647186ecdb298718781f18a9fc56ccf6
Laura Dodge <laura.luv.grace@gmail.com>
2022-08-04 15:25:02 +1000

    learnt numbers and math

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@
  */
 // this logs poop to the console
 console.log("poop");
+
+// this is a number data type
+console.log(Math.round((68 + 1 - 1 / 1) * 1.0147058823529));
+
+// learning how to randomise numbers
+console.log(Math.floor(Math.random() * 100 + 1));
